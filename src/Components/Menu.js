b203bc5bbd6376c37cd4bfc3e5d28c6335aaeec8
blees12345 <adriang1702@gmail.com>
2022-08-30 14:34:30 -0700

    toggle switch for menu
    Co-authored-by: alfredo-saenz <alfredosaenz104@gmail.com>

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Button } from '@material-tailwind/react';
-
+import { useState } from 'react';
 function Menu(props) {
+      const [show, setShow] = useState(false)
+ const handleClick = (event) => {
+		// 👇️ toggle shown state
+		setShow((current) => !current);
+
+		// 👇️ or simply set it to true
+		// setIsShown(true);
+ };
 	return (
 		<div>
+			{show ? <h1>Hello World !</h1> : null}
+			<toggle>
+				<Button onClick={handleClick}>Burritos</Button>{' '}
+			</toggle>
 
-            <Button>Burritos</Button>
-            <Button>Tortas</Button>
-            <Button>La Chiquita Favorites</Button>
-            <Button>Tacos/Quesadillas</Button>
-            <Button>Platillos/Menudo</Button>
+			<Button>Tortas</Button>
+			<Button>La Chiquita Favorites</Button>
+			<Button>Tacos/Quesadillas</Button>
+			<Button>Platillos/Menudo</Button>
 		</div>
 	);
 }
