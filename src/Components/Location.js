f5e4a21c2c3518403f362280a30625e30ed50c7f
Adrian Guia <adriang1702@gmail.com>
2022-09-08 09:07:25 -0700

    added google maps react
    Co-Authored-By: alfredo-saenz <91204720+alfredo-saenz@users.noreply.github.com>

--- src/Components/Location.js
+++ src/Components/Location.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import {
+	GoogleMap,
+	useJsApiLoader,
+    LoadScript,
+    useLoadScript
+} from '@react-google-maps/api';
 
 function Location(props) {
-    return (
-        <div>
-<h1>Location</h1>
-        </div>
-    );
-}
+   const { isLoaded } = useLoadScript({
+			googleMapsApiKey: process.env.REACT_APP_API_KEY,
+		});
 
+  
+
+    if(!isLoaded) return <div>Loading...</div>;
+    return <Map />
+     function Map(){
+return  (
+<GoogleMap zoom={10} center= {{lat:44, lng: -80}} mapContainerClassName="map-container"></GoogleMap>
+     );
+     
+}}
 export default Location;
