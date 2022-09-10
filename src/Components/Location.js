05f00d5d2128e7a69157feadd1bbb34cc4c2a49f
Adrian Guia <adriang1702@gmail.com>
2022-09-10 14:42:10 -0700

    added info window
    Co-Authored-By: alfredo-saenz <91204720+alfredo-saenz@users.noreply.github.com>

--- src/Components/Location.js
+++ src/Components/Location.js
@@ -10,8 +10,17 @@ function Location(props) {
    const { isLoaded } = useLoadScript({
 			googleMapsApiKey: process.env.REACT_APP_API_KEY,
 		});
-
-     const [selectedElement, setSelectedElement] = useState(null);
+const infoWindowStyle = {
+	position: 'relative',
+	bottom: 150,
+	left: '-45px',
+	width: 220,
+	backgroundColor: 'white',
+	boxShadow: '0 2px 7px 1px rgba(0, 0, 0, 0.3)',
+	padding: 10,
+	fontSize: 14,
+	zIndex: 100,
+};
 
     if(!isLoaded) return <div>Loading...</div>;
     return <Map />
@@ -23,11 +32,13 @@ return (
 			zoom={15}
 			center={{ lat: 39.11583, lng: -94.64421 }}
 			mapContainerClassName='map-container'>
-		<Marker position={{ lat: 39.11583, lng: -94.64421 }} visible={true}>
-            
-        </Marker>
+			<Marker position={{ lat: 39.11583, lng: -94.64421 }} visible={true}>
+				<InfoWindow position={{ lat: 39.11583, lng: -94.64421 }} visible={true}>
+				
+            <div>CLick here for Directions</div>
+				</InfoWindow>
+			</Marker>
 		</GoogleMap>
-        
 	</div>
 );
      
