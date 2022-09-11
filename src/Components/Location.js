139bb0b993bfa2b2b26b80a5ebc67af9389b011d
Adrian Guia <adriang1702@gmail.com>
2022-09-11 10:59:53 -0700

    edited marker
    Co-Authored-By: alfredo-saenz <91204720+alfredo-saenz@users.noreply.github.com>

--- src/Components/Location.js
+++ src/Components/Location.js
@@ -35,11 +35,13 @@ return (
 			<Marker position={{ lat: 39.11583, lng: -94.64421 }} visible={true}>
 				<InfoWindow position={{ lat: 39.11583, lng: -94.64421 }} visible={true}>
 					<div>
-						<div> Burritos La Chiquita</div>
+						<h1 className='restaurant-name'> Burritos La Chiquita</h1>
+						<h2>1328 Minnesota Ave, Kansas City, KS 66102</h2>
 						<a
+							className='link'
 							target='_blank'
 							href='https://www.google.com/maps/place/Burritos+La+Chiquita/@39.11583,-94.64421,18z/data=!4m5!3m4!1s0x0:0xf7868bc695e8c0e2!8m2!3d39.115896!4d-94.6445251?hl=en-US'>
-							Click for more info
+							View on Google Maps
 						</a>
 					</div>
 				</InfoWindow>
