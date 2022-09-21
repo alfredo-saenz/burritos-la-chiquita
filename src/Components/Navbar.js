78048033348cfb02fb5e667ec43ed6fd909b265d
alfredo-saenz <alfredosaenz104@gmail.com>
2022-09-21 17:58:10 -0500

    designed the layout of the homepage and location
    Co-authored-by: Adrian Guia <Adriang1702@gmail.com>
    Co-authored-by: alfredo-saenz <alfredosaenz104@gmail.com>

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
 		<div>
-			<nav className='bg-gray-700'>
+			<nav className='bg-transparent'>
 				<div className='max-w-6xl mx-auto px-4 sm:px-6 lg:px-8'>
 					<div className='flex items-center justify-between h-16'>
 						<div className='flex items-center'>
@@ -16,7 +16,7 @@ function Navbar() {
 								</Link>
 							</div>
 							<div className='hidden md:block'>
-								<div className='ml-10 flex items-baseline space-x-4'>
+								<div className='ml-80 flex items-baseline space-x-4'>
 									<Link
 										to='/menu'
 										className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>
@@ -115,7 +115,7 @@ function Navbar() {
 				</Transition>
 			</nav>
 
-			
+
 		</div>
 	);
 }
